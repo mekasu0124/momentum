61b03d669874157206472b31cb8dc7ba00d36405
mekasu0124 <mek0124@proton.me>
2026-05-28 15:16:00 -0500

    created model

--- core/models/task.py
+++ core/models/task.py
@@ -1,8 +1,13 @@
 from sqlalchemy import (
     Column,
+    DateTime,
+    UUID,
     String,
-    Integer
+    Boolean
 )
+
+from datetime import datetime
+from uuid import uuid4
 
 from ..database.db import get_base
 
@@ -14,10 +19,34 @@ class Task(Base):
     __tablename__ = 'tasks'
 
     id = Column(
-        Integer,
+        UUID(as_uuid=True),
         primary_key = True,
         index = True,
-        autoincrement = True
+        default = lambda: uuid4()
     )
 
-    content = Column(String)
+    task = Column(
+        String,
+        unique = True,
+        index = True
+    )
+
+    is_completed = Column(
+        Boolean,
+        index = True,
+        default = False
+    )
+
+    created_at = Column(
+        DateTime,
+        index = True,
+        default = lambda: datetime.now()
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "task": self.task,
+            "is_completed": self.is_completed,
+            "created_at": self.created_at
+        }
