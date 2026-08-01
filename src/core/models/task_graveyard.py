e04c9887787d93158b337178d60fa5c0d259e5ca
mekasu0124 <mek0124@proton.me>
2026-07-31 19:33:00 -0500

    added relationships

--- src/core/models/task_graveyard.py
+++ src/core/models/task_graveyard.py
@@ -1,5 +1,66 @@
+from sqlalchemy import (
+    Column,
+    UUID,
+    DateTime,
+    String,
+    Text,
+    ForeignKey
+)
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from uuid import uuid4
 
+from ..database.db import get_base
+
+
+Base = get_base()
 
 
 class TaskGraveyard(Base):
     __tablename__ = 'graveyard'
+
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        index=True,
+        unique=True,
+        nullable=False,
+        default=lambda: uuid4()
+    )
+
+    user_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        index=True
+    )
+
+    title = Column(
+        String(75),
+        index=True,
+        unique=True,
+        nullable=False
+    )
+
+    content = Column(
+        Text(255),
+        index=True,
+        nullable=False
+    )
+
+    created_at = Column(
+        DateTime,
+        index=True,
+        nullable=False,
+        default=lambda: datetime.now()
+    )
+
+    updated_at = Column(
+        DateTime,
+        index=True,
+        nullable=True,
+        onupdate=lambda: datetime.now()
+    )
+
+    # Relationship
+    user = relationship("User", back_populates="graveyard_tasks")
--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,5 +1,70 @@
+from sqlalchemy import (
+    Column,
+    UUID,
+    DateTime,
+    String
+)
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from uuid import uuid4
 
+from ..database.db import get_base
+
+
+Base = get_base()
 
 
 class User(Base):
     __tablename__ = 'users'
+
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        index=True,
+        unique=True,
+        nullable=False,
+        default=lambda: uuid4()
+    )
+
+    email_address = Column(
+        String,
+        index=True,
+        unique=True,
+        nullable=False
+    )
+
+    username = Column(
+        String,
+        index=True,
+        unique=True,
+        nullable=False
+    )
+
+    hashed_password = Column(String)
+
+    created_at = Column(
+        DateTime,
+        index=True,
+        nullable=False,
+        default=lambda: datetime.now()
+    )
+
+    updated_at = Column(
+        DateTime,
+        index=True,
+        nullable=True,
+        onupdate=lambda: datetime.now()
+    )
+
+    # Relationships
+    tasks = relationship(
+        "Task",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
+
+    graveyard_tasks = relationship(
+        "TaskGraveyard",
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
